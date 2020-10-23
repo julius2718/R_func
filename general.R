95a5e388f8c5d5dd279b38c725bbf1cd9b0c7cd4
Yugo Nakazawa <yuhgon0928@gmail.com>
2020-10-23 12:36:35 +0900

    Add clear workspace

--- general.R
+++ general.R
@@ -10,3 +10,27 @@ save_plot <- function(obj = NULL, file_name = "", width = NULL, height = NULL) {
   print(obj)
   dev.off()
 }
+
+# Clear Workspace--------------------------------------------------------------
+# https://htsuda.net/archives/1545 (as of: 2020-08-15)
+clear_console <- function() {
+  # Clear console messages
+  cat("\014")
+}
+
+clear_plots <- function() {
+  # Clear plots
+  if (dev.cur() > 1) dev.off()
+}
+
+clear_workspace <- function() {
+  # Clear global workspace
+  rm(list = ls(envir = globalenv()), envir = globalenv())
+}
+
+clear_all <- function() {
+  # Clear console, plots, and workspace
+  clear_console()
+  clear_plots()
+  clear_workspace()
+}
