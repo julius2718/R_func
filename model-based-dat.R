d74ad3b2565b02dd3c6a020a4c41c6fe55572810
Yugo Nakazawa <yuhgon0928@gmail.com>
2020-11-02 18:25:14 +0900

    Fix coding rule errors

--- model-based-dat.R
+++ model-based-dat.R
@@ -1,31 +1,82 @@
 
-# With Noise on Path Coefficients
-with_path_noise <- function(
-  N = 10000, path_pi = list(), path_lam = list(), seed = 0
+path_fixed <- function(
+  n = 10000, path_pi = list(), path_lam = list(), seed = 0
 ) {
   set.seed(seed)
   # Exogenous Variables
-  f1 <- rnorm(N, 0, 1)
+  f1 <- rnorm(n, 0, 1)
   ## Observation Errors
   for (i in 1:9) {
-    assign(sprintf("e%d", i), rnorm(N, 0, 1))
+    assign(sprintf("e%d", i), rnorm(n, 0, 1))
   }
   ## Latent Errors
   for (i in 2:3) {
-    assign(sprintf("d%d", i), rnorm(N, 0, 1))
+    assign(sprintf("d%d", i), rnorm(n, 0, 1))
   }
   # Path Coefficients
   ## Structural Equations
-  pi_21 <- rnorm(N, path_pi$pi_21, 0.5)
-  pi_32 <- rnorm(N, path_pi$pi_32, 0.5)
-  pi_31 <- rnorm(N, path_pi$pi_31, 0.5)
+  pi_21 <- path_pi$pi_21
+  pi_32 <- path_pi$pi_32
+  pi_31 <- path_pi$pi_31
   ## Observation Equations
   no <- 1
   for (i in 1:3) {
     for (j in 1:3) {
       assign(
         sprintf("lam_%d%d", no, i),
-        rnorm(N, path_lam[sprintf("lam_%d%d", no, i)][[1]], 0.5)
+        path_lam[sprintf("lam_%d%d", no, i)][[1]]
+      )
+      no <- no + 1
+    }
+  }
+  no <- 1
+  # Endogenous Variables
+  ## Latent Variables
+  f2 <- f1 * pi_21 + d2
+  f3 <- f1 * pi_31 + f2 * pi_32 + d3
+  ## Observed Variables
+  dat_list <- list()
+  for (i in 1:3) {
+    for (j in 1:3) {
+      from <- get(sprintf("f%d", i))
+      lam <- get(sprintf("lam_%d%d", no, i))
+      e <- get(sprintf("e%d", no))
+      assign(sprintf("obs%d", no), (3.5 + from * lam + e))
+      dat_list[sprintf("obs%d", no)] <- list(get(sprintf("obs%d", no)))
+      no <- no + 1
+    }
+  }
+  return(as.data.frame(dat_list))
+}
+
+
+# With Noise on Path Coefficients
+with_path_noise <- function(
+  n = 10000, path_pi = list(), path_lam = list(), seed = 0
+) {
+  set.seed(seed)
+  # Exogenous Variables
+  f1 <- rnorm(n, 0, 1)
+  ## Observation Errors
+  for (i in 1:9) {
+    assign(sprintf("e%d", i), rnorm(n, 0, 1))
+  }
+  ## Latent Errors
+  for (i in 2:3) {
+    assign(sprintf("d%d", i), rnorm(n, 0, 1))
+  }
+  # Path Coefficients
+  ## Structural Equations
+  pi_21 <- rnorm(n, path_pi$pi_21, 0.5)
+  pi_32 <- rnorm(n, path_pi$pi_32, 0.5)
+  pi_31 <- rnorm(n, path_pi$pi_31, 0.5)
+  ## Observation Equations
+  no <- 1
+  for (i in 1:3) {
+    for (j in 1:3) {
+      assign(
+        sprintf("lam_%d%d", no, i),
+        rnorm(n, path_lam[sprintf("lam_%d%d", no, i)][[1]], 0.5)
       )
       no <- no + 1
     }
